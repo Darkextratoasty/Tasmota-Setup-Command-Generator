3421202b29e82de9fd3047f1db0dae3d6093445c
darkextratoasty <joshuaeblackburn@gmail.com>
2023-08-05 01:44:12 +0000

    added files

---
+++ cmd-gen.py
@@ -0,0 +1,26 @@
+import yaml
+
+with open("config.yml", "r") as stream:
+    try:
+        config = yaml.safe_load(stream)
+        cmd = "Backlog SSId " + config['wifi_ssid']
+        cmd += "; Password " + config['wifi_password']
+        cmd += "; Hostname " + config['device_hostname']
+        cmd += "; MqttHost " + config['mqtt_host']
+        cmd += "; MqttPort " + config['mqtt_port']
+        cmd += "; MqttUser " + config['mqtt_username']
+        cmd += "; MqttPassword " + config['mqtt_password']
+        cmd += "; Topic " + config['mqtt_topic']
+        cmd += "; DeviceName " + config['device_name']
+        cmd += "; FriendlyName " + config['friendly_name']
+        if config['configure_ntp']:
+            cmd += "; NtpServer1 " + config['ntp_server']
+            cmd += "; Timezone " + config['ntp_timezone']
+            cmd += "; TimeStd " + config['ntp_timestd']
+            cmd += "; TimeDst " + config['ntp_timedst']
+        cmd += "; Template " + config['template']
+        cmd += "; Module 0"
+
+        print(cmd)
+    except yaml.YAMLError as exc:
+        print(exc)
